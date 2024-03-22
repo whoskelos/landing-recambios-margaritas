e92b2e5bc08cad0c27a56c1a63378f860279426b
Kelvin Guerrero <kelvinguerrero2199@outlook.es>
2024-03-22 20:22:05 +0100

    add brand section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				"loop-scroll" : "loop-scroll 4s linear infinite"
+			},
+			keyframes: {
+				"loop-scroll": {
+					from: { transform: "translateX(0)" },
+					to: { transform: "translateX(-100%)" },
+				},
+			},
+		},
 	},
 	plugins: [],
 }
